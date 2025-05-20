aed0c184affcbd621a214b3a3e9641fd49b34614
JvHaeckel <joaohaeckel@gmail.com>
2025-05-20 17:54:40 -0300

    MOBI

--- testando.py
+++ testando.py
@@ -1,11 +1,16 @@
 
+# Exemplo básico: Coletar os títulos de notícias de um site. Vamos começar com um exemplo simples: extrair manchetes do site da Mobi.
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.mobibrasil.com.br/'
 
 
+resposta = requests.get(url) # pega o HTML da página.
 
+soup = BeautifulSoup(resposta.text, 'html.parser') # transforma esse HTML em uma estrutura navegável.
 
-
-
-
-
+titulos = soup.find_all('a', class_='feed-post-link')
 
 
